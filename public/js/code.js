4492cec05c2cb4397e69d84868cecf42c89657ff
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-02-15 15:40:49 -0500

    Implement login logic

---
+++ public/js/code.js
@@ -0,0 +1,58 @@
+const urlBase = 'http://52.205.195.135/lampstack/LAMPAPI';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin() {
+    userId = 0;
+    firstName = "";
+    lastName = "";
+
+    let login = document.getElementById("loginName").value;
+    let password = document.getElementById("loginPassword").value;
+    //	var hash = md5( password );
+
+    document.getElementById("loginResult").innerHTML = "";
+
+    let tmp = { login, password };
+    //	var tmp = {login:login,password:hash};
+    let jsonPayload = JSON.stringify(tmp);
+    let url = urlBase + '/Login.' + extension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if (userId < 1) {
+                    document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                    return;
+                }
+
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+                saveCookie();
+
+                window.location.href = "color.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err) {
+        document.getElementById("loginResult").innerHTML = err.message;
+    }
+
+}
+
+function saveCookie() {
+    let minutes = 20;
+    let date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
